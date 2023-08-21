3762424927d3df6c7c520e95c859846412a2122e
mdoderovic <79202643+mdoderovic@users.noreply.github.com>
2023-08-21 11:49:05 +0200

    Readme updated

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { RecoilRoot } from 'recoil';
 import { RouterProvider } from 'react-router-dom';
@@ -11,6 +10,7 @@ import router from './global/router';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
     <RecoilRoot>
